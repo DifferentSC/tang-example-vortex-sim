71ba94b9fb9075e898d6d4705341d8f3371ca55b
Gyewon Lee <strayyyyyy@gmail.com>
2015-08-06 14:08:12 +0800

    Add missing 'Inject' annotation to dummy tasks

--- src/main/java/lee/gy/tasks/Mapper.java
+++ src/main/java/lee/gy/tasks/Mapper.java
@@ -3,10 +3,14 @@ package lee.gy.tasks;
 import lee.gy.ExampleMain;
 import org.apache.reef.tang.annotations.Parameter;
 
+import javax.inject.Inject;
+
 /**
  * A dummy Mapper class
  */
 public class Mapper extends Task {
+
+  @Inject
   public Mapper(
       @Parameter(ExampleMain.ArriveTime.class) int arriveTime,
       @Parameter(ExampleMain.Duration.class) int duration) {
--- src/main/java/lee/gy/tasks/Reducer.java
+++ src/main/java/lee/gy/tasks/Reducer.java
@@ -3,11 +3,14 @@ package lee.gy.tasks;
 import lee.gy.ExampleMain;
 import org.apache.reef.tang.annotations.Parameter;
 
+import javax.inject.Inject;
+
 /**
  * A dummy Reducer class
  */
 public class Reducer extends Task {
 
+  @Inject
   public Reducer(
       @Parameter(ExampleMain.ArriveTime.class) int arriveTime,
       @Parameter(ExampleMain.Duration.class) int duration) {
